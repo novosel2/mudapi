e8a584a73b5372a987216cf518cd883ac5013053
Mateo Novosel <mateo.novosel2@gmail.com>
2025-04-30 22:39:04 +0200

    Room entities

---
+++ Mud.Infrastructure/Data/Migrations/20250430203832_AddRoom.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mud.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Characters",
+                newName: "AccountUsername");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AccountUsername",
+                table: "Characters",
+                newName: "Username");
+        }
+    }
+}
