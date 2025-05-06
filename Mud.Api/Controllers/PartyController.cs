79eb68230e6bd49e9c4a4ff2e5b90fab3bf16653
Mateo Novosel <mateo.novosel2@gmail.com>
2025-05-06 20:03:17 +0200

    Boilerplate for Party Controller

---
+++ Mud.Api/Controllers/PartyController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mud.Api.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/parties")]
+public class PartyController : ControllerBase
+{
+    public PartyController()
+    {
+        
+    }
+    
+    
+    // GET: /api/parties
+    
+    [HttpGet]
+    public IActionResult GetParties()
+    {
+        return Ok();
+    }
+    
+    
+    // POST: /api/parties
+
+    [HttpPost]
+    public IActionResult AddParty()
+    {
+        return Ok();
+    }
+    
+    
+    // DELETE: /api/parties
+    
+    [HttpDelete]
+    public IActionResult DeleteParty()
+    {
+        return Ok();
+    }
+    
+    
+    // POST: /api/parties/join-party/{id}
+
+    [HttpPost("join-party/{id:guid}")]
+    public IActionResult JoinParty(Guid id)
+    {
+        return Ok();
+    }
+    
+    
+    // POST: /api/parties/leave-party/{id}
+    [HttpPost("leave-party")]
+    public IActionResult LeaveParty()
+    {
+        return Ok();
+    }
+}
