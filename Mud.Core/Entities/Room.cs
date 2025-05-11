8cdb79a8adc4e7f10b5ccb9b52cbccf967e60a5a
Mateo Novosel <mateo.novosel2@gmail.com>
2025-05-11 23:14:34 +0200

    Add positions to rooms

--- Mud.Core/Entities/Room.cs
+++ Mud.Core/Entities/Room.cs
@@ -1,4 +1,6 @@
-﻿namespace Mud.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mud.Core.Entities;
 
 public class Room
 {
@@ -7,4 +9,9 @@ public class Room
     public string Name { get; set; } = string.Empty;
     
     public string Description { get; set; } = string.Empty;
+    
+    [Required]
+    public int PosX { get; set; }
+    [Required]
+    public int PosY { get; set; }
 }
---
+++ Mud.Infrastructure/Data/Migrations/20250511211355_AddPositionToRooms.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mud.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPositionToRooms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PosX",
+                table: "Rooms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PosY",
+                table: "Rooms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PosX",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "PosY",
+                table: "Rooms");
+        }
+    }
+}
